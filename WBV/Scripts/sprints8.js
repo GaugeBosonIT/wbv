e062372fc5ba3ddfba4c88c516eb433347018cb2
Martin Peschke <martin@friendfund.com>
2011-12-05 15:50:15 +0100

    transitions improvements

--- WBV/Scripts/sprints8.js
+++ WBV/Scripts/sprints8.js
@@ -22,7 +22,6 @@ _.extend(window.sprints8, {
   }
   , isEmail: function (email) { return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(email) }
   , Navigator: function () {
-    this.current_page = 0;
     this.to = function (n) {
       window.scrollTo(0, 1);
       if (n != this.current_page){
@@ -32,7 +31,6 @@ _.extend(window.sprints8, {
                   , topage = $(pages.get(n))
                   , activePageClass = "active-page"
                   , transEnd = function (e) {
-                    window.scrollTo(0, 1);
                     frompage.removeClass(activePageClass).removeClass(" out slide " + reverseClass);
                     topage.removeClass("in slide" + reverseClass);
                     topage.unbind('webkitAnimationEnd');
@@ -45,6 +43,7 @@ _.extend(window.sprints8, {
           topage.bind('oAnimationEnd', transEnd, this);
           frompage.addClass("slide out" + reverseClass);
           topage.addClass(activePageClass).addClass("slide in " + reverseClass);
+          window.scrollTo(0, 1);
         } else {
           frompage.removeClass(activePageClass);
           topage.addClass(activePageClass);
