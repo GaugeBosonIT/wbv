a8e6d07b5d921ad846c9490c947be7921c1b9963
Harry McCarney <hmccarney@gmail.com>
2011-12-05 10:16:49 +0100

    returning json from send gift and redirecting to index from claim

--- WBV/Models/Result.cs
+++ WBV/Models/Result.cs
@@ -6,18 +6,22 @@ using System.Xml.Serialization;
 using System.Runtime.Serialization;
 using System.Web.Mvc;
 using WBV.Interfaces;
+using System.ServiceModel;
 
 
 namespace WBV.Models
 {
+     
     [Serializable]
     public class Result
     {
-
+         
         [XmlAttribute("proc_name")]
         public string proc_name { get; set; }
+  
         [XmlAttribute("status")]
         public int status { get; set; }
+       
         [XmlElement("User")]
         public User user{get; set; }
     }
